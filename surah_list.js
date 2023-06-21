0791d5840c1259e50d08fd954ca116e306740835
seth-ossidian <antonygoldn909@gamil.com>
2023-06-21 11:44:55 +0100

    ;-;

---
+++ surah_list.js
@@ -0,0 +1,828 @@
+//surah list
+let All_surah = [
+  {
+  name: "Al-Fatihah",
+  path: "quran/0.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Baqarah",
+  path: "quran/1.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Aali Imran",
+  path: "quran/2.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "An-Nisa",
+  path: "quran/3.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Ma'idah",
+  path: "quran/4.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-An'am",
+  path: "quran/5.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-A'raf",
+  path: "quran/6.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Anfal",
+  path: "quran/7.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "At-Tawbah",
+  path: "quran/8.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Yunus",
+  path: "quran/9.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Hud",
+  path: "quran/10.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Yusuf",
+  path: "quran/11.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Ar-Ra'd",
+  path: "quran/12.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Ibrahim",
+  path: "quran/13.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Hijr",
+  path: "quran/14.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "An-Nahl",
+  path: "quran/15.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Isra",
+  path: "quran/16.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Kahf",
+  path: "quran/17.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Maryam",
+  path: "quran/18.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Ta-Ha",
+  path: "quran/19.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Anbiya",
+  path: "quran/20.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Hajj",
+  path: "quran/21.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Mu'minun",
+  path: "quran/22.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "An-Nur",
+  path: "quran/23.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Furqan",
+  path: "quran/24.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Ash-Shu'ara",
+  path: "quran/25.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "An-Naml",
+  path: "quran/26.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Qasas",
+  path: "quran/27.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Ankabut",
+  path: "quran/28.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Ar-Rum",
+  path: "quran/29.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Luqman",
+  path: "quran/30.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "As-Sajdah",
+  path: "quran/31.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Ahzab",
+  path: "quran/32.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Saba",
+  path: "quran/33.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Fatir",
+  path: "quran/34.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Ya-Sin",
+  path: "quran/35.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "As-Saffat",
+  path: "quran/36.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Sad",
+  path: "quran/37.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Az-Zumar",
+  path: "quran/38.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Ghafir",
+  path: "quran/39.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Fussilat",
+  path: "quran/40.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Ash-Shura",
+  path: "quran/41.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Az-Zukhruf",
+  path: "quran/42.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Ad-Dukhan",
+  path: "quran/43.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Jathiyah",
+  path: "quran/44.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Ahqaf",
+  path: "quran/45.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Muhammad",
+  path: "quran/46.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Fath",
+  path: "quran/47.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Hujurat",
+  path: "quran/48.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Qaf",
+  path: "quran/49.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Adh-Dhariyat",
+  path: "quran/50.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "At-Tur",
+  path: "quran/51.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "An-Najm",
+  path: "quran/52.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Qamar",
+  path: "quran/53.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Ar-Rahman",
+  path: "quran/54.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Waqi'ah",
+  path: "quran/55.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Hadid",
+  path: "quran/56.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Mujadila",
+  path: "quran/57.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Hashr",
+  path: "quran/58.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Mumtahanah",
+  path: "quran/59.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "As-Saff",
+  path: "quran/60.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Jumu'ah",
+  path: "quran/61.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Munafiqun",
+  path: "quran/62.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "At-Taghabun",
+  path: "quran/63.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "At-Talaq",
+  path: "quran/64.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "At-Tahrim",
+  path: "quran/65.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Mulk",
+  path: "quran/66.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Qalam",
+  path: "quran/67.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Haqqah",
+  path: "quran/68.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Ma'arij",
+  path: "quran/69.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Nuh",
+  path: "quran/70.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Jinn",
+  path: "quran/71.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Muzzammil",
+  path: "quran/72.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Muddathir",
+  path: "quran/73.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Qiyamah",
+  path: "quran/74.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Insan",
+  path: "quran/75.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Mursalat",
+  path: "quran/76.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "An-Naba",
+  path: "quran/77.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "An-Nazi'at",
+  path: "quran/78.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Abasa",
+  path: "quran/79.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "At-Takwir",
+  path: "quran/80.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Infitar",
+  path: "quran/81.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Mutaffifin",
+  path: "quran/82.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Inshiqaq",
+  path: "quran/83.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Buruj",
+  path: "quran/84.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "At-Tariq",
+  path: "quran/85.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-A'la",
+  path: "quran/86.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Ghashiyah",
+  path: "quran/87.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Fajr",
+  path: "quran/88.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Balad",
+  path: "quran/89.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Ash-Shams",
+  path: "quran/90.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Lail",
+  path: "quran/91.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Ad-Duha",
+  path: "quran/92.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Ash-Sharh",
+  path: "quran/93.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "At-Tin",
+  path: "quran/94.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-'Alaq",
+  path: "quran/95.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Qadr",
+  path: "quran/96.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Bayyinah",
+  path: "quran/97.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Az-Zalzalah",
+  path: "quran/98.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-'Adiyat",
+  path: "quran/99.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Qari'ah",
+  path: "quran/100.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "At-Takathur",
+  path: "quran/101.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-'Asr",
+  path: "quran/102.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Humazah",
+  path: "quran/103.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Fil",
+  path: "quran/104.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Quraysh",
+  path: "quran/105.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Ma'un",
+  path: "quran/106.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Kawthar",
+  path: "quran/107.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Kafirun",
+  path: "quran/108.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "An-Nasr",
+  path: "quran/109.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Masad",
+  path: "quran/110.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Ikhlas",
+  path: "quran/111.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "Al-Falaq",
+  path: "quran/112.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  },
+  
+  {
+  name: "An-Nas",
+  path: "quran/113.mp3",
+  img: "images/1.jpg",
+  singer: "Qarya",
+  }
+];
+/*you can add more surah & images from you computer*/
+
+
+/*tracks*/
+let tracks = document.querySelector('.tracks');
+
+//creating a list or generating Html
+for (let i = 0; i < All_surah.length; i++) {
+
+  let Html = ` <div class="surah">
+      <div class="img">
+      <img src="${All_surah[i].img}"/>
+      </div>
+      <div class="more">
+      <audio src="${All_surah[i].path}" id="quran"></audio>
+      <div class="surah_info">
+         <p id="title">${All_surah[i].name}</p>
+         <p>${All_surah[i].singer}</p>
+      </div>
+      <button id="play_btn"><i class="fa fa-angle-right" aria-hidden="true"></i></button>
+      </div>
+    </div>`;
+
+  tracks.insertAdjacentHTML("beforeend", Html);
+};
+
+
+/*please follow all the rules so that you do not face any problem*/
