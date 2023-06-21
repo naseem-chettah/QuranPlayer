fa6991ebf8e7840fe3eaf56908d8029e7b395f99
seth-ossidian <antonygoldn909@gamil.com>
2023-06-21 13:55:07 +0100

    ;-;

--- song_list.js
+++ song_list.js
@@ -4,798 +4,798 @@ let All_song = [
   name: "Al-Fatihah",
   path: "music/0.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Baqarah",
   path: "music/1.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Aali Imran",
   path: "music/2.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "An-Nisa",
   path: "music/3.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Ma'idah",
   path: "music/4.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-An'am",
   path: "music/5.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-A'raf",
   path: "music/6.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Anfal",
   path: "music/7.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "At-Tawbah",
   path: "music/8.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Yunus",
   path: "music/9.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Hud",
   path: "music/10.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Yusuf",
   path: "music/11.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Ar-Ra'd",
   path: "music/12.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Ibrahim",
   path: "music/13.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Hijr",
   path: "music/14.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "An-Nahl",
   path: "music/15.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Isra",
   path: "music/16.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Kahf",
   path: "music/17.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Maryam",
   path: "music/18.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Ta-Ha",
   path: "music/19.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Anbiya",
   path: "music/20.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Hajj",
   path: "music/21.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Mu'minun",
   path: "music/22.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "An-Nur",
   path: "music/23.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Furqan",
   path: "music/24.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Ash-Shu'ara",
   path: "music/25.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "An-Naml",
   path: "music/26.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Qasas",
   path: "music/27.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Ankabut",
   path: "music/28.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Ar-Rum",
   path: "music/29.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Luqman",
   path: "music/30.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "As-Sajdah",
   path: "music/31.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Ahzab",
   path: "music/32.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Saba",
   path: "music/33.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Fatir",
   path: "music/34.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Ya-Sin",
   path: "music/35.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "As-Saffat",
   path: "music/36.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Sad",
   path: "music/37.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Az-Zumar",
   path: "music/38.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Ghafir",
   path: "music/39.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Fussilat",
   path: "music/40.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Ash-Shura",
   path: "music/41.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Az-Zukhruf",
   path: "music/42.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Ad-Dukhan",
   path: "music/43.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Jathiyah",
   path: "music/44.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Ahqaf",
   path: "music/45.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Muhammad",
   path: "music/46.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Fath",
   path: "music/47.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Hujurat",
   path: "music/48.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Qaf",
   path: "music/49.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Adh-Dhariyat",
   path: "music/50.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "At-Tur",
   path: "music/51.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "An-Najm",
   path: "music/52.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Qamar",
   path: "music/53.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Ar-Rahman",
   path: "music/54.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Waqi'ah",
   path: "music/55.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Hadid",
   path: "music/56.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Mujadila",
   path: "music/57.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Hashr",
   path: "music/58.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Mumtahanah",
   path: "music/59.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "As-Saff",
   path: "music/60.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Jumu'ah",
   path: "music/61.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Munafiqun",
   path: "music/62.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "At-Taghabun",
   path: "music/63.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "At-Talaq",
   path: "music/64.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "At-Tahrim",
   path: "music/65.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Mulk",
   path: "music/66.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Qalam",
   path: "music/67.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Haqqah",
   path: "music/68.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Ma'arij",
   path: "music/69.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Nuh",
   path: "music/70.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Jinn",
   path: "music/71.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Muzzammil",
   path: "music/72.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Muddathir",
   path: "music/73.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Qiyamah",
   path: "music/74.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Insan",
   path: "music/75.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Mursalat",
   path: "music/76.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "An-Naba",
   path: "music/77.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "An-Nazi'at",
   path: "music/78.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Abasa",
   path: "music/79.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "At-Takwir",
   path: "music/80.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Infitar",
   path: "music/81.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Mutaffifin",
   path: "music/82.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Inshiqaq",
   path: "music/83.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Buruj",
   path: "music/84.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "At-Tariq",
   path: "music/85.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-A'la",
   path: "music/86.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Ghashiyah",
   path: "music/87.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Fajr",
   path: "music/88.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Balad",
   path: "music/89.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Ash-Shams",
   path: "music/90.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Lail",
   path: "music/91.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Ad-Duha",
   path: "music/92.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Ash-Sharh",
   path: "music/93.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "At-Tin",
   path: "music/94.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-'Alaq",
   path: "music/95.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Qadr",
   path: "music/96.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Bayyinah",
   path: "music/97.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Az-Zalzalah",
   path: "music/98.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-'Adiyat",
   path: "music/99.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Qari'ah",
   path: "music/100.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "At-Takathur",
   path: "music/101.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-'Asr",
   path: "music/102.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Humazah",
   path: "music/103.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Fil",
   path: "music/104.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Quraysh",
   path: "music/105.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Ma'un",
   path: "music/106.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Kawthar",
   path: "music/107.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Kafirun",
   path: "music/108.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "An-Nasr",
   path: "music/109.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Masad",
   path: "music/110.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Ikhlas",
   path: "music/111.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "Al-Falaq",
   path: "music/112.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   },
   
   {
   name: "An-Nas",
   path: "music/113.mp3",
   img: "images/1.jpg",
-  singer: "Qarya",
+  singer: "Abdelaziz Sheim",
   }
 ];
 /*you can add more song & images from you computer*/
